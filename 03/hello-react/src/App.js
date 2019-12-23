1f5f5b5883046f7430bca581ad909ff4984a82c1
jeongjaeeom <jeongjae.eom@gmail.com>
2019-12-23 11:40:41 +0900

    Add

--- 03/hello-react/src/App.js
+++ 03/hello-react/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import MyComponent from "./MyComponent";
+import Say from "./Counter";
 
-function App() {
+/*function App() {
   return (
     <MyComponent name="React" favoriteNumber={1}>
       리액트
     </MyComponent>
   )
-}
+}*/
+
+const App = () => {
+  return <Say />;
+};
 
 export default App;
