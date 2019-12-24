709effc7ddfeff6e895f44c516fc82ea75759293
jeongjaeeom <jeongjae.eom@gmail.com>
2019-12-24 18:14:38 +0900

    Add hooks

--- 08/hooks-tutorial/src/App.js
+++ 08/hooks-tutorial/src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import Counter from "./Counter";
+import Info from "./Info";
 
-function App() {
+const App = () => {
+  const [visible, setVisible] = useState(false);
   return (
-    <Counter/>
+    <div>
+      <button
+        onClick={() => {
+          setVisible(!visible)
+        }}
+      >
+        {visible ? '숨기기' : '보이기'}
+      </button>
+      <hr/>
+      {visible && <Info/>}
+    </div>
   );
-}
+};
 
 export default App;
