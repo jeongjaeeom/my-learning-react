efbd6fe0b5d21fe76544df3b764fd2dd0dbd3088
jeongjaeeom <jeongjae.eom@gmail.com>
2020-01-06 23:16:14 +0900

    First commit

--- 13/router-tutorial/src/App.js
+++ 13/router-tutorial/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link, Route} from 'react-router-dom';
+import {Link, Route, Switch} from 'react-router-dom';
 import './App.css';
 import Home from "./Home";
 import About from "./About";
@@ -22,10 +22,21 @@ const App = () => (
         <Link to="/history">History 예제</Link>
       </li>
     </ul>
-    <Route path="/" component={Home} exact={true}/>
-    <Route path={['/about', '/info']} component={About}/>
-    <Route path="/profiles" component={Profiles}/>
-    <Route path="/history" component={HistorySample}/>
+    <Switch>
+      <Route path="/" component={Home} exact={true}/>
+      <Route path={['/about', '/info']} component={About}/>
+      <Route path="/profiles" component={Profiles}/>
+      <Route path="/history" component={HistorySample}/>
+      <Route
+        // path를 따로 정의하지 않으면 모든
+        render={({location}) => (
+          <div>
+            <h2>이 페이지는 존재하지 않습니다:</h2>
+            <p>{location.pathname}</p>
+          </div>
+        )}
+      />
+    </Switch>
   </div>
 );
 
