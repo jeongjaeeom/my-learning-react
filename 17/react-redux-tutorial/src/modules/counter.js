f169a2902e28e018dd9a412260412a5a8af9853b
jeongjaeeom <jeongjae.eom@gmail.com>
2020-01-23 17:34:03 +0900

    Add components

--- 17/react-redux-tutorial/src/modules/counter.js
+++ 17/react-redux-tutorial/src/modules/counter.js
@@ -1,14 +1,27 @@
+import {createAction, handleActions} from 'redux-actions';
+
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
 
-export const increase = () => ({type: INCREASE});
-export const decrease = () => ({type: DECREASE});
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
+
+// export const increase = () => ({type: INCREASE});
+// export const decrease = () => ({type: DECREASE});
 
 const initialState = {
   number: 0
 };
 
-function counter(state = initialState, action) {
+const counter = handleActions(
+    {
+      [INCREASE]: (state, action) => ({number: state.number + 1}),
+      [DECREASE]: (state, action) => ({number: state.number - 1})
+    },
+    initialState
+);
+
+/*function counter(state = initialState, action) {
   switch (action.type) {
     case INCREASE:
       return {
@@ -21,6 +34,6 @@ function counter(state = initialState, action) {
     default:
       return state;
   }
-}
+}*/
 
 export default counter;
